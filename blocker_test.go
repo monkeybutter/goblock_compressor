b6d3ade284cda82995275412561123c7f8a30c7b
Pablo <pablo@Pablos-MacBook-Air.local>
2015-10-09 21:22:57 +1100

    Benchmark created

---
+++ blocker_test.go
@@ -0,0 +1,57 @@
+package blockcompressor
+
+import (
+	"testing"
+	"fmt"
+	"encoding/binary"
+	"math"
+	"os"
+)
+
+func Float64bytes(float float64) []byte {
+	bits := math.Float64bits(float)
+	bytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bytes, bits)
+	return bytes
+}
+
+func create_float64_file(size int, filename string) {
+	// open output file
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	for i := 0; i < size; i++ {
+		file.Write(Float64bytes(float64(i)))
+	}
+}
+
+func BenchmarkBlockUnblock(b *testing.B) {
+	create_float64_file(1000000, "input.bin")
+    	b.ResetTimer()
+     	for n := 0; n < b.N; n++ {
+		readerBlocks := blockGenerator("input.bin", 256, 64, 4)
+		compressorBlocks := blockProcessor(blockProcessor(readerBlocks, shuff), comp)
+		blockWriter(compressorBlocks, "output.bin")
+	}
+	b.StopTimer()
+	
+	err := os.Remove("input.bin")
+
+      	if err != nil {
+         	 fmt.Println("Error deleteing input.bin", err)
+      	}
+
+	err = os.Remove("output.bin")
+
+      	if err != nil {
+         	 fmt.Println("Error deleteing output.bin", err)
+      	}
+}
